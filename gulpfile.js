bbf1543aadde542a4b5cc665fe837464581e256b
Data Chand <data.chand@gmail.com>
2015-08-22 23:26:15 +0530

    Watch Added

--- gulpfile.js
+++ gulpfile.js
@@ -10,6 +10,7 @@
         usemin = require('gulp-usemin'),
         connect = require('gulp-connect'),
         copy = require('gulp-copy'),
+        watch = require('gulp-watch'),
         sourcemaps = require('gulp-sourcemaps'),
         del = require('del');
 
@@ -18,7 +19,7 @@
         gulp.src('src/index.html')
             .pipe(usemin({
                 libraries: [uglify(), rev()],
-                javascripts: [sourcemaps.init(), jshint(), jshint.reporter('jshint-stylish'), ngAnnotate(), uglify(), rev(), sourcemaps.write('./')],
+                javascripts: [sourcemaps.init(), jshint(), jshint.reporter('jshint-stylish'), uglify(), rev(), sourcemaps.write('./')],
                 stylesheets: [rev()]
             }))
             .pipe(gulp.dest('dist/'));
@@ -34,6 +35,13 @@
 
     gulp.task('clean', function () {
         del('dist/**/*');
+    });
+
+    gulp.task('scripts', function () {
+        gulp.src('src/app/**/*.js')
+            .pipe(watch('src/app/**/*.js'))
+            .pipe(jshint())
+            .pipe(jshint.reporter('jshint-stylish'));
     });
 
     gulp.task('build', ['minification']);
